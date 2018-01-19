32503769309cf11a5bc3b8774cb0418c976c1a0d
Мартышин Семен <smar@1c.ru>
2018-01-19 14:34:25 +0300

    рефакторинг исходные код клиента 1С

--- main.py
+++ main.py
@@ -9,28 +9,25 @@ app = Flask(__name__)
 
 @app.route('/GetHelp', methods=['POST'])
 def get_help():
-    users_query = request.json['query']
-    normalized_query = semantic_engine.canonize_words(users_query.split())
-    min_distance = 100
-    closest_label = ''
+    normalized_user_query = semantic_engine.canonize_words(request.json['query'].split())
+    min_distance, closest_label = 100, ''
     all_labels = labels()
     for label in all_labels:
         normalized_label = semantic_engine.canonize_words(all_labels[label]['keywords'].split())
-        label_distance = model.wmdistance(normalized_query, normalized_label)
+        label_distance = model.wmdistance(normalized_user_query, normalized_label)
         if label_distance < min_distance:
-            min_distance = label_distance
-            closest_label = label
+            min_distance, closest_label = label_distance, label
 
     return jsonify({
         'answer': closest_label,
-        'normalized_user_query': normalized_query,
+        'normalized_user_query': normalized_user_query,
         'form': all_labels[closest_label]['form_name']
     })
 
 
 if __name__ == '__main__':
     """
-    before start need to download corpora from link below  to /data folder 
+    before start need to download corpora from link below and write path to corpora in settings.py 
     http://rusvectores.org/static/models/rusvectores4/ruwikiruscorpora/ruwikiruscorpora_upos_skipgram_300_2_2018.vec.gz
 
     must have libraries: (pip install 'library_name')
